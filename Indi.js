2754df7f6ba3a32a3d7814d5737d1bd00c443a30
Ludovic P <pludov@nnx.com>
2017-08-06 15:07:52 +0200

    Maintains the mapping from indi_driver => HW type

--- Indi.js
+++ Indi.js
@@ -2,11 +2,9 @@
 
 const xmlbuilder = require('xmlbuilder');
 const net = require('net');
-const sax = require('sax');
 
 const Promises = require('./Promises');
-
-
+const Xml2JSONParser = require('./Xml2JSONParser.js');
 
 const schema = {
       
@@ -365,80 +363,9 @@ class IndiConnection {
     }
 
     newParser(onMessage) {
+        var parser = Xml2JSONParser(schema, 2, onMessage);
+
         var self = this;
-        var parser = sax.parser(true)
-        var level = 2;
-        var currentNodes = [];
-        var currentSchemas = [];
-        var currentLevel = 0;
-
-        parser.onopentag = function (node) {
-            var name = node.name;
-            currentLevel++;
-            if (currentLevel >= level) {
-                var id = currentLevel - level;
-
-                if (id == 0) {
-                    currentSchemas[id] = schema[name];
-                } else {
-                    currentSchemas[id] = currentSchemas[id - 1][name];
-                }
-                if (currentSchemas[id] == undefined) {
-                    currentSchemas[id] = {};
-                }
-
-                var currentSchema = currentSchemas[id];
-                
-                var newNode = {};
-          
-                if (id == 0) {
-                    newNode.$$=name;
-                } else {
-                    if (currentSchema.$isArray) {
-                        currentNodes[id - 1][name].push(newNode);
-                    } else {
-                        currentNodes[id - 1][name] = newNode;
-                    }
-                }
-                for(var key in node.attributes) {
-                    newNode['$' + key] = node.attributes[key];
-                }
-          
-                for(var childId in currentSchema) {
-                    if (currentSchema[childId].$isArray) {
-                        newNode[childId] = [];
-                    }
-                }
-
-                currentNodes[id] = newNode;
-            }
-        };
-
-        parser.ontext = function(text) {
-            var id = currentLevel - level;
-            if (id >= 0) {
-                if (currentSchemas[id].$notext) {
-                    return;
-                }
-                // Hackish: trim but keep possibility to have \n
-                text = text.replace(/^\n/, '');
-                text = text.replace(/\n *$/, '');
-                currentNodes[id].$_ = text;
-            }
-        }
-
-        parser.onclosetag = function (name) {
-            currentLevel--;
-            if (currentLevel ==  level - 1) {
-                // End of current message
-                console.log('finished message parsing: ' + JSON.stringify(currentNodes[0]));
-                onMessage(currentNodes[0]);
-            }
-            if (currentLevel >= level - 1) {
-                delete currentNodes[currentLevel - (level - 1)];
-            }
-        };
-
 
         parser.onerror = function(e) {
             console.log('xml error: ' + e);
--- IndiManager.js
+++ IndiManager.js
@@ -4,8 +4,10 @@
 
 'use strict';
 
+const Xml2JSONParser = require('./Xml2JSONParser.js');
 const {IndiConnection} = require('./Indi');
 const Promises = require('./Promises');
+const fs = require('fs');
 
 function has(obj, key) {
     return Object.prototype.hasOwnProperty.call(obj, key);
@@ -19,20 +21,112 @@ function clear(obj) {
     }
 }
 
+const DriverXmlSchema = {
+    driversList: {
+        devGroup: {
+            $isArray:true,
+            $notext: true,
+            device: {
+                $isArray:true
+            }
+        }
+    }
+};
+
 class IndiManager {
 
     constructor(app, appStateManager) {
+        var self = this;
         this.appStateManager = appStateManager;
         this.appStateManager.getTarget().indiManager = {
             // connecting, connected, error
             status: "connecting",
-            deviceTree: {}
+            deviceTree: {},
+            // Maps indi drivers to group
+            driverToGroup: {}
         }
+
+        
 
         this.currentStatus = this.appStateManager.getTarget().indiManager;
 
+        this.initConfiguration('indiManager', {
+            driverPath: '/usr/share/indi/'
+        });
+
+        // List configuration settings
+        this.appStateManager.addSynchronizer(['configuration', 'indiManager', 'driverPath'], () => {self.readDrivers();}, true);
+
         this.lifeCycle = this.buildLifeCycle();
         this.lifeCycle.start();
+    }
+
+    // FIXME: move up in class hierarchy
+    initConfiguration(name, defaults)
+    {
+        function createChildObject(parent, name)
+        {
+            if (!Object.prototype.hasOwnProperty.call(parent, name)) {
+                parent[name] = {};
+            }
+        }
+
+        function sync(target, value) {
+            for(var key of Object.keys(defaults)) {
+                var value = defaults[key];
+                if (Object.prototype.hasOwnProperty.call(target, value)) {
+                    // Recurse in sub objects ?
+                    continue;
+                }
+                // We can put here since proxy mechanism will deep clone
+                target[key] = value;
+            }
+        }
+
+        var root = this.appStateManager.getTarget();
+        createChildObject(root, 'configuration');
+        createChildObject(root.configuration, name);
+        sync(root.configuration[name], defaults);
+    }
+
+    readDrivers()
+    {
+        var path = this.appStateManager.getTarget().configuration.indiManager.driverPath;
+        
+        var driverToGroup = {};
+
+        fs.readdirSync(path).filter(file => (file.match(/\.xml$/) && !file.match(/_sk\.xml$/))).forEach((file)=> {
+            file = path + '/' + file;
+            console.log('Found driver: ' + file);
+            
+            try {
+                function onMessage(e) {
+                    if (e.$$ != 'driversList') {
+                        console.warn('Ignored xml driver content: ' + JSON.stringify(e, null, 2));
+                        return;
+                    }
+                    try {
+                        for(var group of e.devGroup)
+                        {
+                            for(var device of group.device) {
+                                var driver = device.driver.$_;
+                                driverToGroup[driver] = group.$group;
+                            }
+                        }
+                    } catch(e) {
+                        console.error('Failed to parse xml drivers', e.stack || e);
+                    }
+                }
+                var content = fs.readFileSync(file);
+                var parser = Xml2JSONParser(DriverXmlSchema, 1, onMessage);
+                parser.write(content)
+                parser.close();
+            } catch(e) {
+                console.log('Unable to parse ' + file, e.stack || e);
+            }
+        });
+
+        this.currentStatus.driverToGroup = driverToGroup;
     }
 
     refreshStatus()
