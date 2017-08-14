55fd0b6dc5b3c3397af2b808165505dcc8b0b1b4
Ludovic P <pludov@nnx.com>
2017-08-14 22:47:04 +0200

    The shoot button listen for current shoots

--- ui/src/CameraView.js
+++ ui/src/CameraView.js
@@ -2,6 +2,8 @@ import React, { Component, PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import { notifier, BackendStatus } from './Store';
 import { connect } from 'react-redux';
+
+import { atPath } from './shared/JsonPath';
 import FitsViewer from './FitsViewer';
 import PromiseSelector from './PromiseSelector';
 import CameraSettingsView from './CameraSettingsView';
@@ -14,12 +16,60 @@ const CameraSelector = connect((store)=> ({
             availables: store.backend.camera.availableDevices
 }))(PromiseSelector);
 
+class ShootBton extends PureComponent {
+    constructor(props) {
+        super(props);
+        this.shoot = this.shoot.bind(this);
+    }
+
+    render() {
+        return <input disabled={(!this.props.available) || this.props.running} type="button" onClick={this.shoot} className="shootBton" value="Shoot"/>
+    }
+
+    shoot() {
+        var self = this;
+        this.props.app.serverRequest({
+            method: 'shoot'
+        }).then((rslt)=>
+        {
+            console.log('got rslt:' + JSON.stringify(rslt));
+            self.props.onSuccess(rslt);
+        }).start();
+    }
+
+    static mapStateToProps(store, ownProps) {
+        var result = {
+            available: false
+        }
+        var active = atPath(store, ownProps.activePath);
+        if (active === null) return result;
+
+        // Check if exposure is present
+        var deviceNode = atPath(store, '$.backend.indiManager.deviceTree[' + JSON.stringify(active) + "].CCD_EXPOSURE");
+        if (deviceNode === undefined) return result;
+        
+        result.available = true;
+
+        result.running = (deviceNode.$state == 'Busy');
+
+        return result;
+    }
+}
+
+ShootBton = connect(ShootBton.mapStateToProps)(ShootBton);
+
+ShootBton.propTypes = {
+    onSuccess: PropTypes.func.isRequired,
+    activePath: PropTypes.string.isRequired,
+    app: PropTypes.any.isRequired
+}
+
 class CameraView extends PureComponent {
 
     constructor(props) {
         super(props);
         this.state = {url: 'test.jpg'};
-        this.shoot = this.shoot.bind(this);
+        this.setPhoto = this.setPhoto.bind(this);
         this.connect = this.connect.bind(this);
     }
 
@@ -41,20 +91,16 @@ class CameraView extends PureComponent {
                 <FitsViewer src={this.state.url}/>
             </div>
             <div className="ButtonBar">
-                <input type="button" onClick={this.shoot} value="Shoot"/>
+                <ShootBton
+                        activePath="$.backend.camera.selectedDevice"
+                        onSuccess={this.setPhoto} value="Shoot"
+                        app={this.props.app}/>
             </div>
         </div>);
     }
 
-    shoot() {
-        var self = this;
-        this.props.app.serverRequest({
-            method: 'shoot'
-        }).then((rslt)=>
-        {
-            console.log('got rslt:' + JSON.stringify(rslt));
-            self.setState({url : 'fitsviewer/fitsviewer.cgi?path=' + encodeURIComponent(rslt.path)});
-        }).start();
+    setPhoto(rslt) {
+        this.setState({url : 'fitsviewer/fitsviewer.cgi?path=' + encodeURIComponent(rslt.path)});
     }
 
     connect() {
