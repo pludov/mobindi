56e700a76d5ff6eb96f049aa74fb4c05f5693f27
Ludovic P <pludov@nnx.com>
2017-08-26 09:28:37 +0200

    Preliminary rendering of images history

--- ui/src/SequenceView.js
+++ ui/src/SequenceView.js
@@ -6,13 +6,59 @@ import { connect } from 'react-redux';
 import { atPath } from './shared/JsonPath';
 
 
+class ImageListEntry extends PureComponent {
+
+    render() {
+        return <div>{this.props.item.path}</div>;
+    }
+    static mapStateToProps = function(store, ownProps) {
+        return({
+            item: atPath(store, ownProps.uidListPath).byuuid[ownProps.imageUid]
+        })
+    }
+}
+
+ImageListEntry = connect(ImageListEntry.mapStateToProps)(ImageListEntry);
+ImageListEntry.propTypes = {
+    // Path to image details
+    uidListPath: PropTypes.string.isRequired,
+    imageUid: PropTypes.string.isRequired
+}
+
+class ImageList extends PureComponent {
+
+    render() {
+        var content = [];
+        for(var o of this.props.items)
+        {
+            content.push(<ImageListEntry key={o} uidListPath={this.props.uidListPath} imageUid={o}/>);
+        }
+        return <div>{content}</div>;
+    }
+
+    static mapStateToProps = function(store, ownProps) {
+        return({
+            // FIXME: filter, cache
+            items: atPath(store, ownProps.uidListPath).list
+        })
+    }
+}
+
+ImageList = connect(ImageList.mapStateToProps)(ImageList);
+ImageList.propTypes = {
+    // Path to image details
+    uidListPath: PropTypes.string.isRequired
+}
+
 class SequenceView extends PureComponent {
     constructor(props) {
         super(props);
     }
     render() {
         //var self = this;
-        return(<div className="CameraView">Sequence !</div>);
+        return(<div className="CameraView">
+            <ImageList uidListPath="$.backend.camera.images"/>
+        </div>);
     }
 }
 
