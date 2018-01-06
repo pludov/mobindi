1797f4122cf73b9efcfa9c350bf49096ede06bf4
Ludovic P <pludov@nnx.com>
2018-01-06 10:32:43 +0100

    Switched back to streamed baseline jpeg. Much more reactive

--- fitsviewer/fitsviewer.cpp
+++ fitsviewer/fitsviewer.cpp
@@ -58,113 +58,129 @@ void debayer(u_int8_t * data, int width, int height, u_int8_t * target)
 	}
 }
 
-void write_jpeg_file(u_int8_t * grey, int width, int height, int channels)
+
+class JpegWriter
 {
+	int width, height, channels;
 
-	  /* This struct contains the JPEG compression parameters and pointers to
-	   * working space (which is allocated as needed by the JPEG library).
-	   * It is possible to have several such structures, representing multiple
-	   * compression/decompression processes, in existence at once.  We refer
-	   * to any one struct (and its associated working data) as a "JPEG object".
-	   */
-	  struct jpeg_compress_struct cinfo;
-	  /* This struct represents a JPEG error handler.  It is declared separately
-	   * because applications often want to supply a specialized error handler
-	   * (see the second half of this file for an example).  But here we just
-	   * take the easy way out and use the standard error handler, which will
-	   * print a message on stderr and call exit() if compression fails.
-	   * Note that this struct must live as long as the main JPEG parameter
-	   * struct, to avoid dangling-pointer problems.
-	   */
-	  struct jpeg_error_mgr jerr;
-	  /* More stuff */
-	  FILE * outfile;		/* target file */
-	  JSAMPROW row_pointer[32];	/* pointer to JSAMPLE row[s] */
-	  int row_stride;		/* physical row width in image buffer */
+	/* This struct contains the JPEG compression parameters and pointers to
+	 * working space (which is allocated as needed by the JPEG library).
+	 * It is possible to have several such structures, representing multiple
+	 * compression/decompression processes, in existence at once.  We refer
+	 * to any one struct (and its associated working data) as a "JPEG object".
+	 */
+	struct jpeg_compress_struct cinfo;
 
-	  /* Step 1: allocate and initialize JPEG compression object */
+	/* This struct represents a JPEG error handler.  It is declared separately
+	 * because applications often want to supply a specialized error handler
+	 * (see the second half of this file for an example).  But here we just
+	 * take the easy way out and use the standard error handler, which will
+	 * print a message on stderr and call exit() if compression fails.
+	 * Note that this struct must live as long as the main JPEG parameter
+	 * struct, to avoid dangling-pointer problems.
+	 */
+	struct jpeg_error_mgr jerr;
+public:
+	JpegWriter(int w, int h, int channels)
+	{
+		this->width = w;
+		this->height = h;
+		this->channels = channels;
 
-	  /* We have to set up the error handler first, in case the initialization
-	   * step fails.  (Unlikely, but it could happen if you are out of memory.)
-	   * This routine fills in the contents of struct jerr, and returns jerr's
-	   * address which we place into the link field in cinfo.
-	   */
-	  cinfo.err = jpeg_std_error(&jerr);
-	  /* Now we can initialize the JPEG compression object. */
-	  jpeg_create_compress(&cinfo);
+	}
 
-	  /* Step 2: specify data destination (eg, a file) */
-	  /* Note: steps 2 and 3 can be done in either order. */
+	void start()
+	{
+		  /* Step 1: allocate and initialize JPEG compression object */
 
-	  /* Here we use the library-supplied code to send compressed data to a
-	   * stdio stream.  You can also write your own code to do something else.
-	   * VERY IMPORTANT: use "b" option to fopen() if you are on a machine that
-	   * requires it in order to write binary files.
-	   */
-	  jpeg_stdio_dest(&cinfo, stdout);
+		  /* We have to set up the error handler first, in case the initialization
+		   * step fails.  (Unlikely, but it could happen if you are out of memory.)
+		   * This routine fills in the contents of struct jerr, and returns jerr's
+		   * address which we place into the link field in cinfo.
+		   */
+		  cinfo.err = jpeg_std_error(&jerr);
+		  /* Now we can initialize the JPEG compression object. */
+		  jpeg_create_compress(&cinfo);
 
-	  /* Step 3: set parameters for compression */
+		  /* Step 2: specify data destination (eg, a file) */
+		  /* Note: steps 2 and 3 can be done in either order. */
 
-	  /* First we supply a description of the input image.
-	   * Four fields of the cinfo struct must be filled in:
-	   */
-	  cinfo.image_width = width; 	/* image width and height, in pixels */
-	  cinfo.image_height = height;
-	  cinfo.input_components = channels;		/* # of color components per pixel */
-	  cinfo.in_color_space = channels == 1 ? JCS_GRAYSCALE : JCS_RGB; 	/* colorspace of input image */
-	  /* Now use the library's routine to set default compression parameters.
-	   * (You must set at least cinfo.in_color_space before calling this,
-	   * since the defaults depend on the source color space.)
-	   */
-	  jpeg_set_defaults(&cinfo);
-	  /* Now you can set any non-default parameters you wish to.
-	   * Here we just illustrate the use of quality (quantization table) scaling:
-	   */
-	  jpeg_set_quality(&cinfo, 90, FALSE /* don't limit to baseline-JPEG values */);
-	  jpeg_simple_progression(&cinfo);
+		  /* Here we use the library-supplied code to send compressed data to a
+		   * stdio stream.  You can also write your own code to do something else.
+		   * VERY IMPORTANT: use "b" option to fopen() if you are on a machine that
+		   * requires it in order to write binary files.
+		   */
+		  jpeg_stdio_dest(&cinfo, stdout);
 
-	  /* Step 4: Start compressor */
-	//  cinfo.progressive_mode = TRUE;
+		  /* Step 3: set parameters for compression */
 
-	  /* TRUE ensures that we will write a complete interchange-JPEG file.
-	   * Pass TRUE unless you are very sure of what you're doing.
-	   */
-	  jpeg_start_compress(&cinfo, TRUE);
+		  /* First we supply a description of the input image.
+		   * Four fields of the cinfo struct must be filled in:
+		   */
+		  cinfo.image_width = width; 	/* image width and height, in pixels */
+		  cinfo.image_height = height;
+		  cinfo.input_components = channels;		/* # of color components per pixel */
+		  cinfo.in_color_space = channels == 1 ? JCS_GRAYSCALE : JCS_RGB; 	/* colorspace of input image */
+		  /* Now use the library's routine to set default compression parameters.
+		   * (You must set at least cinfo.in_color_space before calling this,
+		   * since the defaults depend on the source color space.)
+		   */
+		  jpeg_set_defaults(&cinfo);
+		  /* Now you can set any non-default parameters you wish to.
+		   * Here we just illustrate the use of quality (quantization table) scaling:
+		   */
 
-	  /* Step 5: while (scan lines remain to be written) */
-	  /*           jpeg_write_scanlines(...); */
+		  jpeg_set_quality(&cinfo, 90, TRUE /* limit to baseline-JPEG values */);
 
-	  /* Here we use the library's state variable cinfo.next_scanline as the
-	   * loop counter, so that we don't have to keep track ourselves.
-	   * To keep things simple, we pass one scanline per call; you can pass
-	   * more if you wish, though.
-	   */
-	  row_stride = channels * width;	/* JSAMPLEs per row in image_buffer */
+		  // For progressive, use: jpeg_simple_progression(&cinfo);
+		  // But in that case, the compression will not be streamed.
 
-	  int y = 0;
-	  while(y < height) {
-		  int count = 0;
-		  for(int i = 0; i < 32 && y < height; ++i) {
-			  row_pointer[i] = grey + y * row_stride;
-			  y++;
-			  count++;
-		  }
-		  (void) jpeg_write_scanlines(&cinfo, row_pointer, count);
-	  }
+		  /* Step 4: Start compressor */
+		  /* TRUE ensures that we will write a complete interchange-JPEG file.
+		   * Pass TRUE unless you are very sure of what you're doing.
+		   */
+		  jpeg_start_compress(&cinfo, TRUE);
+	}
 
-	  /* Step 6: Finish compression */
+	void writeLines(uint8_t * grey, int height) {
+		JSAMPROW row_pointer[32];	/* pointer to JSAMPLE row[s] */
+		int row_stride;		/* physical row width in image buffer */
 
-	  jpeg_finish_compress(&cinfo);
-	  /* After finish_compress, we can close the output file. */
-	  fclose(outfile);
+		/* Step 5: while (scan lines remain to be written) */
+		/*           jpeg_write_scanlines(...); */
 
-	  /* Step 7: release JPEG compression object */
+		/* Here we use the library's state variable cinfo.next_scanline as the
+		 * loop counter, so that we don't have to keep track ourselves.
+		 * To keep things simple, we pass one scanline per call; you can pass
+		 * more if you wish, though.
+		 */
+		row_stride = channels * width;	/* JSAMPLEs per row in image_buffer */
 
-	  /* This is an important step since it will release a good deal of memory. */
-	  jpeg_destroy_compress(&cinfo);
+		int y = 0;
+		while(y < height) {
+			int count = 0;
+			for(int i = 0; i < 32 && y < height; ++i) {
+				row_pointer[i] = grey + y * row_stride;
+				y++;
+				count++;
+			}
+			(void) jpeg_write_scanlines(&cinfo, row_pointer, count);
+		}
+	}
 
-	  /* And we're done! */
-}
+	void finish()
+	{
+		/* Step 6: Finish compression */
+		jpeg_finish_compress(&cinfo);
+
+
+		/* Step 7: release JPEG compression object */
+		/* This is an important step since it will release a good deal of memory. */
+		jpeg_destroy_compress(&cinfo);
+	}
+
+};
+
 
 void write_png_file(u_int8_t * grey, int width, int height)
 {
@@ -334,9 +350,17 @@ int main (int argc, char ** argv) {
 	int h = storage->h;
 	std::string bayer = storage->getBayer();
 
-	u_int8_t * result = new u_int8_t[w * h];
 	uint16_t * data = storage->data;
 
+	bool color = bayer.length() > 0;
+
+	cgicc::HTTPResponseHeader header("HTTP/1.1", 200, "OK");
+	header.addHeader("Content-Type", "image/jpeg");
+	cout << header;
+
+	JpegWriter writer(w / (color ? 2 : 1), h / (color ? 2 : 1), color > 0 ? 3 : 1);
+	writer.start();
+	fflush(stdout);
 	int nbpix = w * h;
 
 	// do histogram for each channel !
@@ -349,12 +373,29 @@ int main (int argc, char ** argv) {
 			levels[i][2]= channelStorage->getLevel(high);
 		}
 
+		u_int8_t * result = new u_int8_t[w * 32];
+		u_int8_t * superPixel = (u_int8_t*)malloc(3 * (w * 32 / 4));
 
-		for(int i = 0; i < 4; ++i) {
-			int offset = (i & 1) + ((i & 2) >> 1) * w;
-			int hist = RawDataStorage::getRGBIndex(bayer[i]);
-			applyScaleBayer(data + offset, w, h, levels[hist][0], levels[hist][1], levels[hist][2], result + offset);
+		int basey = 0;
+		while(basey < h) {
+			int yleft = h - basey;
+			if (yleft > 32) yleft = 32;
+
+			for(int i = 0; i < 4; ++i) {
+				int bayerOffset = (i & 1) + ((i & 2) >> 1) * w;
+				int hist = RawDataStorage::getRGBIndex(bayer[i]);
+				applyScaleBayer(data + basey * w + bayerOffset, w, yleft, levels[hist][0], levels[hist][1], levels[hist][2], result + bayerOffset);
+			}
+
+			debayer(result, w, yleft, superPixel);
+
+			writer.writeLines(superPixel, yleft / 2);
+
+			basey += 32;
 		}
+
+		delete [] result;
+		free(superPixel);
 	} else {
 		auto channelStorage = histogramStorage->channel(0);
 
@@ -362,26 +403,21 @@ int main (int argc, char ** argv) {
 		int med = channelStorage->getLevel((low + high) / 2);
 		int max = channelStorage->getLevel(high);
 		fprintf(stderr, "levels are %d %d %d", min, med, max);
-		applyScale(data, w, h, min, med, max, result);
+
+		u_int8_t * result = new u_int8_t[w * 32];
+
+		int basey = 0;
+		while(basey < h) {
+			int yleft = h - basey;
+			if (yleft > 32) yleft = 32;
+			applyScale(data + basey * w, w, yleft, min, med, max, result);
+			writer.writeLines(result, yleft);
+
+			basey += 32;
+		}
+		delete(result);
 	}
+	writer.finish();
 	histogram->release();
-	cgicc::HTTPResponseHeader header("HTTP/1.1", 200, "OK");
-	header.addHeader("Content-Type", "image/jpeg");
-	cout << header;
-
-	// Let's bin 2x2
-	if (bayer.length() > 0) {
-		u_int8_t * superPixel = (u_int8_t*)malloc(3 * (w * h / 4));
-		debayer(result, w, h, superPixel);
-		// DO super pixel !
-		write_jpeg_file(superPixel, w / 2, h / 2, 3);
-		free(superPixel);
-
-	} else {
-		write_jpeg_file(result, w, h, 1);
-	}
-
-	delete [] result;
-
 	return 0;
 }
