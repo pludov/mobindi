f9b11018b2dbf8c517bde287afaf1d8dc70ca49a
Ludovic P <pludov@nnx.com>
2019-05-26 14:59:07 +0200

    Fix app.test.tsx

--- ui/src/App.test.tsx
+++ ui/src/App.test.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import * as Store from './Store';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={Store.getStore()}>
+      <App />
+    </Provider>,
+  div);
 });
