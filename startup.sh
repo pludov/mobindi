6a8b54d366aa745798be880b82bf24484051191f
Ludovic P <pludov@nnx.com>
2019-06-14 20:40:20 +0200

    Fix restart from startup.sh

--- startup.sh
+++ startup.sh
@@ -12,18 +12,21 @@ if [ -f ./mobindi.conf ]; then
     . ./mobindi.conf
 fi
 
+# Leave reasonable time to die
+while pkill -U "$UID" -fx "node dist/app.js"; do
+	echo "Killing previous instance" >&2
+	sleep 0.25
+done
+
 if [ "$LOGDIR" != "" ]; then
 	echo "Logging into $LOGDIR"
 	exec < /dev/null
-	exec > >( multilog '.s9999999' "$LOGDIR" )
+	exec > >( multilog 's9999999' "$LOGDIR" )
 	exec 2>&1
 fi
 
 
 # Startup nginx
 ./nginx/nginx.sh
-
-# Leave reasonable time to die
-( pkill -U "$UID" -fx "node dist/app.js" && sleep 0.5 ) || /bin/true
 
 exec npm start "$@"
