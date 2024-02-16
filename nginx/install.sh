08101450f0e8eac877e349ae689fe8d6320a5198
Ludovic P <pludow@gmail.com>
2024-02-16 22:32:50 +0100

    Run under .local domain, using appropriate certificate

--- nginx/install.sh
+++ nginx/install.sh
@@ -15,18 +15,9 @@ fi
 [[ ":$PATH:" != *":/usr/sbin:"* ]] && PATH="${PATH}:/usr/sbin"
 
 which nginx > /dev/null || (echo "Installation of nginx required" >&2 ; sudo apt install nginx)
+which openssl > /dev/null || (echo "Installation of openssl required" >&2; sudo apt install openssl)
+which capsh > /dev/null || (echo "Installation of libcap2-bin required" >&2; sudo apt install libcap2-bin)
+which avahi-daemon > /dev/null || (echo "Installation of avahi-daemon required" >&2; sudo apt install avahi-daemon)
 
-if [ ! -f nginx-selfsigned.key ] \
-        || [ ! -f nginx-selfsigned.crt ] \
-        || [ "$(find nginx-selfsigned.key -mtime +360)" ] \
-        || [ "$(find nginx-selfsigned.crt -mtime +360)" ]; then
-    which openssl > /dev/null || (echo "Installation of openssl required" >&2; sudo apt install openssl)
+./cert.sh
 
-    echo "SSL certificate required" >&2
-
-    openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-            -subj "/C=GB/ST=London/L=London/O=Global Security/OU=IT Department/CN=example.com" \
-            -keyout /tmp/nginx-selfsigned.key -out /tmp/nginx-selfsigned.crt
-
-    mv /tmp/nginx-selfsigned.key /tmp/nginx-selfsigned.crt .
-fi
--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -8,12 +8,17 @@ cd "$INSTALL_DIR"
 
 LOGDIR=""
 
+# Default port is 443
+PUBLIC_PORT="${PUBLIC_PORT:-443}"
+PORT="${PORT:-8080}"
+
 if [ -f ../mobindi.conf ]; then
     . ../mobindi.conf
 fi
 
-
 [[ ":$PATH:" != *":/usr/sbin:"* ]] && PATH="${PATH}:/usr/sbin"
+
+./cert.sh
 
 if [ -f "nginx.pid" ]; then
     if kill -HUP "`cat nginx.pid`" ; then
@@ -23,9 +28,20 @@ if [ -f "nginx.pid" ]; then
 fi
 
 CONF=nginx.conf
-if [ "${PORT+x}" ]; then
-	sed -e "s/:8080/:$PORT/g" nginx.conf > actual-nginx.conf
+if [ "${PORT:-}" != 8080 ] || [ "${PUBLIC_PORT}" != 8443 ]; then
+	sed -e "s/:8080/:$PORT/g" -e "s/:8443/:$PUBLIC_PORT/g" nginx.conf > actual-nginx.conf
 	CONF=actual-nginx.conf
 fi
 
-nginx -p "$PWD" -c ./$CONF 2> /dev/null
+
+COMMAND=(nginx -p "$PWD" -c "./$CONF")
+# Check if port is lower than 1024
+if [ ${PUBLIC_PORT:-9999} -lt 1024 ]; then
+    printf -v cmd '%q ' "${COMMAND[@]}"
+    echo "Running with elevated privileges: $cmd" >&2
+    # cmd="ls -al /proc/self/fd /dev/pts"
+    # | cat is required to ensure the output is sent to the terminal if there is one.
+    sudo -E capsh --keep=1 --caps="cap_setuid,cap_setgid,cap_setpcap+ep cap_net_bind_service+eip" --user="$USER" --addamb=cap_net_bind_service -- -c "$cmd 2> /dev/null" < /dev/null | ( cat & )
+else
+    "${COMMAND[@]}" 2> /dev/null
+fi
