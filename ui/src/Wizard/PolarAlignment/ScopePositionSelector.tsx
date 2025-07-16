029c16a6597f33d4167c7d5a46077d49bfab8e84
Ludovic P <pludov@nnx.com>
2025-07-16 09:50:45 +0200

    Introduce a viewer for scope position in the sky

---
+++ ui/src/Wizard/PolarAlignment/ScopePositionSelector.tsx
@@ -0,0 +1,216 @@
+import { deepEqual } from '../../shared/Obj';
+import React from 'react';
+import '../../AstrometryView.css';
+import * as Store from "../../Store";
+import SkyProjection from '../../SkyAlgorithms/SkyProjection';
+import * as PolarAlignment from '../../SkyAlgorithms/PolarAlignment';
+import './ScopePositionSelector.css';
+import * as IndiUtils from '../../IndiUtils';
+import { defaultMemoize } from 'reselect';
+
+type InputProps = {
+    currentScope: string;
+    moveAllowed: boolean;
+};
+type MappedProps = {
+    gradient: string[][]|undefined;
+    scopeAltAz: undefined|{alt: number, az: number};
+}
+type Props = InputProps & MappedProps;
+
+
+
+
+class ScopePositionSelector extends React.PureComponent<Props> {
+    
+    constructor(props:Props) {
+        super(props);
+    }
+
+    renderGradient(level: number, values: Array<string>) {
+
+
+        let angleStep = 360 / values.length;
+        let background = values.map((c, i) => `${c} ${i * angleStep}deg ${(i + 1) * angleStep}deg`).join(', ');
+
+        
+        const style = { "--level": level, background: `conic-gradient(${background})` } as React.CSSProperties;
+
+        return <div
+                    key={`histo-${level}`}
+                    className='polar_align_sky_view_item'
+                    style={style}
+                        
+                    >                    
+
+                </div>;
+    }
+
+    renderScope(altAz: {alt: number, az: number}) {
+        let dst = 0.5 * (90 - altAz.alt) / 90;
+        
+        let relX = 0.5 + dst * Math.sin(altAz.az * Math.PI / 180.0);
+        let relY = 0.5 - dst * Math.cos(altAz.az * Math.PI / 180.0);
+
+        const style = { "--relx": relX, "--rely": relY } as React.CSSProperties;
+
+        return <div
+                    className='polar_align_sky_view_scope'
+                    style={style}
+                    ></div>
+    }
+
+    render() {
+        return <>
+            <div className="PolarAlignExplain">
+                <div className="polar_align_sky_view_container">
+
+                {
+                    this.props.gradient?.map((e, i) => this.renderGradient(i / this.props.gradient!.length, e))
+                }
+                {
+                    this.props.scopeAltAz ? this.renderScope(this.props.scopeAltAz) : null
+                }
+                </div>
+    
+            </div>
+        </>;
+    }
+
+    static getScopePosFromStore(store: Store.Content, scope: string) {
+        const coordVec = !scope ? undefined : IndiUtils.getVectorDesc(store, scope, 'EQUATORIAL_EOD_COORD');
+        const coordRaDec = [ coordVec?.childs.RA?.$_, coordVec?.childs.DEC?.$_].map(IndiUtils.parsePropFloat);
+        
+        if (coordRaDec[0] === undefined || coordRaDec[1] === undefined) {
+            return undefined;
+        }
+
+        return {ra : 15 * coordRaDec[0], dec: coordRaDec[1]};
+    }
+
+    static getGeoCoords(store: Store.Content, scope: string) {
+        const vec = !scope ? undefined : IndiUtils.getVectorDesc(store, scope, "GEOGRAPHIC_COORD");
+
+        const coords = [ vec?.childs.LAT?.$_, vec?.childs.LONG?.$_].map(IndiUtils.parsePropFloat);
+        
+        if (coords[0] === undefined || coords[1] === undefined) {
+            return undefined;
+        }
+
+        return {lat: coords[0], long: coords[1]};
+    }
+
+
+    static buildGradientFromAxis(axis:  {alt:number, az:number}) {
+        let result: string[][];
+
+        result = [];
+        const alt_step = 16;
+        const az_step = 64;
+        for(let alt_id = 0; alt_id < alt_step; ++alt_id) {
+            const alt_values: string[] = [];
+
+            const alt = 90 * (alt_id + 0.5) / alt_step;
+
+            // FIXME: lower the number of az step for higher alt
+            for(let az_id = 0; az_id < az_step; ++az_id) {
+                const az = 360 * (az_id + 0.5) / az_step;
+
+                let e = PolarAlignment.evalPolarAlignmentPosition({alt, az}, axis);
+
+                // Push a color
+                let color;
+                if (e.alt_norm < PolarAlignment.minimumAxeRatio || e.az_norm < PolarAlignment.minimumAxeRatio) {
+                    color = "red";
+                } else if (e.eval_angle < PolarAlignment.orthogonalityThreshold[1]) {
+                    color = "red";
+                } else {
+                    // Ramp between #F0A000 to dark green: #008F00
+                    const src = [ 0xF0, 0xA0, 0x00];
+                    const dst = [ 0x00, 0x8F, 0x00];
+
+                    const linear = (e.eval_angle - PolarAlignment.orthogonalityThreshold[1]) / (90 - PolarAlignment.orthogonalityThreshold[1]);
+                    const ramp = linear * linear;
+
+                    const rgb = src
+                                .map((e, i) => Math.round(e + ramp * (dst[i] - e)))
+                                .map(e=>e.toString(16).padStart(2, '0'))
+                                .join('');
+                    color = `#${rgb}`;
+                }
+                alt_values.push(color);
+            }
+
+            result.push(alt_values);
+        }
+        return result;
+    }
+
+
+    static getAxis(store: Store.Content, props: InputProps) {
+        const geoCoords = ScopePositionSelector.getGeoCoords(store, props.currentScope);
+        if (geoCoords !== undefined) {
+            const axis: {alt:number, az:number}|null|undefined = store.backend.astrometry?.runningWizard?.polarAlignment?.axis ||
+            {
+                alt: geoCoords.lat,
+                az: 0,
+            };
+            return axis;
+        } else {
+            return undefined;
+        }
+    }
+
+    static getScopeAltAz(store: Store.Content, props: InputProps, now: number) {
+        const geoCoords = ScopePositionSelector.getGeoCoords(store, props.currentScope);
+
+        if (!geoCoords) {
+            return undefined;
+        }
+        const raDecScope = ScopePositionSelector.getScopePosFromStore(store, props.currentScope);
+        if (!raDecScope) {
+            return undefined;
+        }
+
+        const zenithRa = SkyProjection.getLocalSideralTime(now, geoCoords.long);
+        const scopeAltAz = SkyProjection.lstRelRaDecToAltAz({relRaDeg: raDecScope.ra - zenithRa, dec: raDecScope.dec}, geoCoords);
+
+        scopeAltAz.alt = Math.round(scopeAltAz.alt);
+        scopeAltAz.az = Math.round(scopeAltAz.az);
+        return scopeAltAz;
+    }
+
+    static roundScopeAltAz(input: { alt:number, az:number}|undefined) {
+        if (!input) return undefined;
+        let scopeAltAz = {...input};
+        scopeAltAz.alt = Math.round(scopeAltAz.alt);
+        scopeAltAz.az = Math.round(scopeAltAz.az);
+        return scopeAltAz;
+    }
+
+    static mapStateToProps() {
+        
+        const computeGradient = defaultMemoize(ScopePositionSelector.buildGradientFromAxis, {
+            equalityCheck: deepEqual,
+            resultEqualityCheck: deepEqual, 
+        });
+
+        const roundScopeAltAz = defaultMemoize(ScopePositionSelector.roundScopeAltAz, {
+            resultEqualityCheck: deepEqual
+        });
+        
+        return (store: Store.Content, props: InputProps):MappedProps => {
+            const axis = ScopePositionSelector.getAxis(store, props);
+            const now = new Date().getTime();
+            const scopeAltAz = roundScopeAltAz(ScopePositionSelector.getScopeAltAz(store, props, now));
+
+            return {
+                gradient: axis ? computeGradient(axis) : undefined,
+                scopeAltAz
+            }
+        }
+    }
+
+}
+
+export default Store.Connect(ScopePositionSelector);
