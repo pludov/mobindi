adc2a204099e791cb9f3f0c32f32733228edae69
Ludovic P <pludow@gmail.com>
2026-02-08 18:41:37 +0100

    Account startup delay in mount moves

--- PolarAlignmentWizard.ts
+++ PolarAlignmentWizard.ts
@@ -113,48 +113,80 @@ class ImpreciseDirectionChecker {
 }
 
 class AccelerationDetector {
-    maxSpeed: number;
+    maxSpeed: number|undefined;
     startSample: number;
     lastSample: number;
+    accelPhaseSamples: number|undefined;
+    lastTime: number;
     sampleCount: number;
     done: boolean;
     unit: string;
+    startDelay: number;
 
     constructor(firstSample: number, unit: string) {
         this.unit = unit;
         this.lastSample = firstSample;
         this.startSample = firstSample;
-        this.maxSpeed = 0;
+        this.maxSpeed = undefined;
+        this.accelPhaseSamples = undefined;
         this.sampleCount = 1;
         this.done = false;
+        this.startDelay = 0;
+        this.lastTime = 0;
     }
 
     addStep(newSample: number, duration: number) {
-        if (duration <= 0) {
+        if (this.maxSpeed === undefined && newSample == this.startSample) {
+            this.startDelay = duration;
+            this.lastTime = duration;
             return;
         }
-        const newSpeed = Math.abs((newSample - this.startSample) / duration);
-        this.maxSpeed = newSpeed;
+        if (duration <= this.startDelay) {
+            return;
+        }
+        // don't consider samples under 200ms. They will report wrong speed
+        if (duration - this.lastTime  < 0.1) {
+            logger.info(`Ignoring speed sample too close`);
+            return;
+        }
+
+        const newSpeed = (newSample - this.lastSample) / (duration - this.lastTime);
+        this.lastTime = duration;
+        
+        logger.info(`Instantaneous speed is ${newSpeed}`);
         if (this.done) {
+            if (this.maxSpeed == undefined || Math.abs(this.maxSpeed) < Math.abs(newSpeed)) {
+                this.maxSpeed = newSpeed;
+            }
+            this.lastSample = newSample;
             return;
         }
-        this.sampleCount++;
-        if (Math.abs(newSpeed) > this.maxSpeed *1.05) {
+        if (this.maxSpeed === undefined || Math.abs(newSpeed) > Math.abs(this.maxSpeed *1.05)) {
             logger.info(`Still accelerating after ${(duration).toFixed(3)}s - ${this.sampleCount} samples for ${Math.abs(newSample - this.startSample)} ${this.unit}`);
-            this.lastSample = newSample;
-            // this.maxSpeed = newSpeed;
         } else {
             logger.info(`Acceleration stopped after ${(duration).toFixed(3)}s - ${this.sampleCount} samples, over a distance of ${Math.abs(this.lastSample - this.startSample)}`);
+            this.accelPhaseSamples = this.lastSample;
             this.done = true;
+        }
+        this.lastSample = newSample;
+        if (this.maxSpeed == undefined || Math.abs(this.maxSpeed) < Math.abs(newSpeed)) {
+            this.maxSpeed = newSpeed;
         }
     }
 
     getExpectedInertia() {
+        if (this.accelPhaseSamples !== undefined) {
+            return this.accelPhaseSamples;
+        }
+        // Not yet finished
         return this.lastSample - this.startSample;
     }
 
     getSpeed() {
-        return this.maxSpeed;
+        if (this.lastTime <= this.startDelay) {
+            return 0;
+        }
+        return (this.lastSample - this.startSample) / (this.lastTime - this.startDelay);
     }
 }
 
